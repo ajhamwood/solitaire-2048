e74a93c0dc4ed88aa6c6845fe7bf9f925501f692
ajhamwood <ajh@tuta.io>
2019-05-12 16:23:34 +1000

    Refactored PWA and notifications into separate Machines, minor UI tweaks

--- index.js
+++ index.js
@@ -23,92 +23,55 @@ const $ = Object.assign((sel, node = document) => [...node.querySelectorAll(sel)
     let stamp = document.importNode($('template#' + id)[0].content, true);
     return $(dest).map(n => [...stamp.cloneNode(true).childNodes.values()].map(c => n.appendChild(c))) } });
 
-// Page state
-var app = new $.Machine({
+
+// Game functionality
+
+var game = new $.Machine({
+  // Game state
   tableau: null,
   hand: null,
   discard: null,
   highscore: null,
   score: null,
 
+  // Display state
   tableauCoords: [],
   discardCoords: null,
   inHand: {},
   active: null,
   vh: document.body.clientHeight / 100,
 
+  // Game constants
   maxHeight: 8,
   highCard: 2048,
-  wildRate: 1/256,
-
-  version: null,
-  swReady: false,
-  checkUpdate: false,
-  a2hsPrompt: null
+  wildRate: 1/256
 });
 
 // UI Events
+
 $.queries({
   '#game-over button': { click () {
     $('#game-over')[0].classList.remove('active');
     $('div.card').forEach(x => x.remove());
-    app.emit('start')
+    game.emit('start')
   } }
 });
 
 $.targets({
 
   // Window events
-  load () {
-    app.emit('init');
-    app.emit('start')
-  },
 
-  'mousemove touchmove' (e) {
-    let {card} = app.getState().inHand, {clientX, clientY} = e.type === 'touchmove' ? e.touches[0] : e;
-    if (card && !card.nextElementSibling && card.classList.contains('in-hand')) app.emit('move-card', clientX, clientY)
-  },
+  load () { game.emit('init') },
+  'mousemove touchmove' (e) { game.emit('move-card', e) },
+  'mouseup touchend' () { game.emit('drop-card') },
+  resize () { game.emit('resize') },
 
-  'mouseup touchend' (e) {
-    let {card} = app.getState().inHand;
-    if (card && !card.nextElementSibling && card.classList.contains('in-hand')) app.emit('drop-card')
-  },
+  // Game events
 
-  resize () { app.emit('resize') },
-
-  beforeinstallprompt (e) {
-    e.preventDefault();
-    if (sessionStorage.suppressA2hs) return false;
-    app.emit('deferA2hs', e);
-    app.emit('notification', {
-      type: 'install',
-      text: 'Add to home screen / desktop?',
-      okText: 'OK',
-      cancelText: 'Cancel',
-      okHandler (e) {
-        sessionStorage.suppressA2hs = true;
-        app.emit('a2hs')
-      },
-      cancelHandler () { sessionStorage.suppressA2hs = true }
-    })
-  },
-
-  // ServiceWorker events
-  navigator: { serviceWorker: { controllerchange (e) { app.emit('update') } } },
-
-  // Page state events
-  app: {
+  game: {
     init () { //TODO: Tutorial
       $('.highscore')[0].textContent = this.highscore = parseInt(localStorage.highscore) || 0;
-      if ('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js')
-        .then(reg => this.swReady = !reg.installing)
-        .then(() => app.emitAsync('debug')).then(() => {
-          // Check for updates every hour on the hour, every minute in development mode
-          let check = () => ((this.checkUpdate = true), check);
-          /-dev$/.test(this.version) ?
-            setTimeout(() => setInterval(check(), 6e4), 6e4 - Date.now() + new Date().setSeconds(0, 0)) :
-            setTimeout(() => setInterval(check(), 3.6e6), 3.6e6 - Date.now() + new Date().setMinutes(0, 0, 0))
-        })
+      game.emit('start')
     },
 
     start () {
@@ -118,16 +81,15 @@ $.targets({
         discard: ($('.discard text')[0].textContent = 2),
         score: ($('.score')[0].textContent = 0)
       });
-      app.emit('resize');
-      app.emit('reveal');
-      app.emit('reveal');
-      if (this.checkUpdate) navigator.serviceWorker.ready.then(reg => reg.update())
-        .then(() => this.checkUpdate = false)
+      game.emit('resize');
+      game.emit('reveal');
+      game.emit('reveal');
+      pwa.emit('check-update')
     },
 
     resize () {
       Object.assign(this, {
-        vh: document.body.clientHeight / 100,
+        vh: parseFloat(getComputedStyle(document.documentElement).fontSize) / 2,
         tableauCoords: $('.column > :last-of-type').map(el => {
           let {x, y} = el.getBoundingClientRect();
           if (el.nodeName === 'DIV') y += 5 * this.vh;
@@ -142,7 +104,7 @@ $.targets({
 
     reveal () {
       let card = $.load('card', '.hand')[0][0],
-          value = 2 ** (1 + Math.floor(6 * Math.random())), state = this;
+          value = 2 ** (1 + Math.floor(6 * Math.random())), gameState = this;
       if (Math.random() > this.wildRate) card.classList.add('face' + value);
       else {
         value = -1;
@@ -156,24 +118,25 @@ $.targets({
               {x, y} = this.getBoundingClientRect();
           this.classList.remove('activating');
           this.classList.add('in-hand');
-          state.inHand = {layerX: clientX - x, layerY: clientY - y};
-          state.inHand.card = this;
+          gameState.inHand = {layerX: clientX - x, layerY: clientY - y, card: this};
           this.style.left = x + 'px';
           this.style.top = y + 'px'
         }
       }});
-      if ($('.hand > *').length === 1) setTimeout(() => app.emitAsync('pipeCard', card), 25); //BUG: How to wait for redraw in firefox?
+      if ($('.hand > *').length === 1) setTimeout(() => game.emitAsync('animate-card', card), 25); //BUG: How to wait for redraw in firefox?
       else $('.hand')[0].insertBefore(card, $('.hand > :first-child')[0])
     },
 
-    'move-card' (x, y) {
-      let style = this.inHand.card.style, card = this.inHand;
-      style.left = x - card.layerX + 'px';
-      style.top = y - card.layerY + 'px';
+    'move-card' (e) {
+      let {card, layerX, layerY} = this.inHand,
+          {clientX, clientY} = e.type === 'touchmove' ? e.touches[0] : e;
+      if (!card || card.nextElementSibling || !card.classList.contains('in-hand')) return false;
+      card.style.left = clientX - layerX + 'px';
+      card.style.top = clientY - layerY + 'px';
       $('.discard, .column').forEach((el, i) => {
         if (this.active !== null && this.active !== i) return false;
         let obj = i === 4 ? this.discardCoords : this.tableauCoords[i],
-            d = Math.hypot(obj.x - x + card.layerX, obj.y - y + card.layerY);
+            d = Math.hypot(obj.x - clientX + layerX, obj.y - clientY + layerY);
         if (this.active === null && d < this.vh * 6) {
           this.active = i;
           el.classList.add('active');
@@ -186,6 +149,7 @@ $.targets({
 
     'drop-card' () {
       let {card} = this.inHand;
+      if (!card || card.nextElementSibling || !card.classList.contains('in-hand')) return false;
       card.classList.remove('in-hand');
       card.style = null;
       this.inHand = {};
@@ -203,12 +167,12 @@ $.targets({
             y += 5 * this.vh;
             this.tableauCoords[i] = {x, y};
             this.hand.shift();
-            app.emit('reduce', i)
+            game.emit('reduce', i)
           } else if (length === this.maxHeight && (this.tableau[i][this.maxHeight - 1] === value || value === -1)) {
             this.tableau[i].push(value);
             card.remove();
             this.hand.shift();
-            app.emit('reduce', i)
+            game.emit('reduce', i)
           } else return false
         } else if (el.classList.contains('discard')) {
           $('.discard')[0].classList.remove('active');
@@ -216,15 +180,15 @@ $.targets({
           $('text', el)[0].textContent = --this.discard;
           card.remove();
           this.hand.shift();
-          app.emit('check-game-over')
+          game.emit('check-game-over')
         }
         let nextcard = $('.hand > div.card:first-child')[0];
-        if (nextcard) app.emitAsync('pipeCard', nextcard).then(() => app.emit('reveal'));
-        else setTimeout(() => app.emit('reveal'), 600);
+        if (nextcard) game.emitAsync('animate-card', nextcard).then(() => game.emit('reveal'));
+        else setTimeout(() => game.emit('reveal'), 600);
       }
     },
 
-    pipeCard (card) {
+    'animate-card' (card) {
       return $.pipe('handAnimation', () => new Promise(r => {
         card.classList.add('activating');
         setTimeout(() => r(card.classList.remove('activating')), 600)
@@ -242,7 +206,7 @@ $.targets({
           card.classList.remove('wildcard');
           card.classList.add('face' + curval * 2);
           $('.score')[0].textContent = (this.score += curval * 2);
-          app.emit('reduce', i)
+          game.emit('reduce', i)
         } else {
           card.classList.add('reducing');
           this.tableauCoords[i] = {x: -1000, y: -1000};
@@ -255,11 +219,11 @@ $.targets({
             nextcard.classList.add('face' + curval * 2);
             card.remove();
             $('.score')[0].textContent = (this.score += curval * 2);
-            if (curval === this.highCard / 2) app.emit('clear-column', i);
-            else app.emit('reduce', i)
+            if (curval === this.highCard / 2) game.emit('clear-column', i);
+            else game.emit('reduce', i)
           }, 600)
         }
-      } else app.emit('check-game-over')
+      } else game.emit('check-game-over')
     },
 
     'clear-column' (i) {
@@ -285,27 +249,90 @@ $.targets({
         if (this.highscore < this.score) $('.highscore')[0].textContent =
           localStorage.highscore = this.highscore = this.score
       }
-    },
+    }
+  }
 
-    notification (obj) {
-      let note = $.load('notification', '#notifications')[0][0];
-      $('div', note)[0].textContent = obj.text;
-      note.id = obj.type;
-      $('button', note).forEach(el => {
-        let which = el.id;
-        el.textContent = obj[which + 'Text'];
-        el.id = obj.type + '-' + which;
-        $.queries({ ['button#' + el.id]: { click (e) {
-          note.remove();
-          obj[which + 'Handler'].bind(this)(e)
-        } } }, note)
+});
+
+
+// Common functionality
+
+var app = new $.Machine({})
+  .on('notification', function (obj) {
+    let note = $.load('notification', '#notifications')[0][0];
+    $('div', note)[0].textContent = obj.text;
+    note.id = obj.type;
+    $('button', note).forEach(el => {
+      let which = el.id;
+      el.textContent = obj[which + 'Text'];
+      el.id = obj.type + '-' + which;
+      $.queries({ ['button#' + el.id]: { click (e) {
+        note.remove();
+        obj[which + 'Handler'].bind(this)(e)
+      } } }, note)
+    })
+  })
+  .on('debug', function (data) { $('.debug')[0].textContent = data.version });
+
+
+// PWA functionality
+
+var pwa = new $.Machine({
+  version: null,
+  swReady: false,
+  allowUpdate: false,
+  a2hsPrompt: null
+});
+
+$.targets({
+
+  // Window events
+
+  load () {
+    if ('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js')
+      .then(reg => this.swReady = !reg.installing)
+      .then(() => pwa.emitAsync('debug')).then(() => {
+        // Check for updates every hour on the hour, every minute in development mode
+        let check = () => ((this.allowUpdate = true), check);
+        /-dev$/.test(this.version) ?
+          setTimeout(() => setInterval(check(), 6e4), 6e4 - Date.now() + new Date().setSeconds(0, 0)) :
+          setTimeout(() => setInterval(check(), 3.6e6), 3.6e6 - Date.now() + new Date().setMinutes(0, 0, 0))
       })
-    },
+  },
 
+  beforeinstallprompt (e) {
+    e.preventDefault();
+    if (sessionStorage.suppressA2hs) return false;
+    pwa.emit('deferA2hs', e);
+    app.emit('notification', {
+      type: 'install',
+      text: 'Add to home screen / desktop?',
+      okText: 'OK',
+      cancelText: 'Cancel',
+      okHandler (e) {
+        sessionStorage.suppressA2hs = true;
+        pwa.emit('a2hs')
+      },
+      cancelHandler () { sessionStorage.suppressA2hs = true }
+    })
+  },
+
+  // ServiceWorker events
+
+  navigator: { serviceWorker: { controllerchange (e) { pwa.emit('update') } } },
+
+  // PWA events
+
+  pwa: {
     deferA2hs (e) { this.a2hsPrompt = e },
     a2hs () {
       this.a2hsPrompt.prompt();
       this.a2hsPrompt.userChoice.then(() => this.a2hsPrompt = null)
+    },
+
+    'check-update' () {
+      if (this.allowUpdate) navigator.serviceWorker.ready.then(reg => reg.update())
+        .then(() => this.allowUpdate = false)
     },
 
     update () {
@@ -325,9 +352,7 @@ $.targets({
       return $.pipe('debug', () => new Promise((resolve, reject) => this.version === null ? reject() : resolve())
         .catch(recover = () => fetch('version')
           .then(res => res.status === 404 ? recover() : res.text().then(ver => this.version = ver)))
-        .then(() => /-dev$/.test(this.version) && app.emit('debug-actions')))
-    },
-    'debug-actions' () { $('.debug')[0].textContent = this.version }
+        .then(() => /-dev$/.test(this.version) && app.emit('debug', {version: this.version})))
+    }
   }
-
 })
